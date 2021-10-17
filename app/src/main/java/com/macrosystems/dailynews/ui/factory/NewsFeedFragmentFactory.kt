ff6aacbcb307bebc83c5fc79683646face9b92d3
Manuel Crovetto <manuel.crovetto@9to5.software>
2021-10-17 16:29:18 +0200

    There was a bug with the instantiation of fragments with its constructor injected. It was solved by adding a custom NavHostFragment.

--- app/src/main/java/com/macrosystems/dailynews/ui/factory/NewsFeedFragmentFactory.kt
+++ app/src/main/java/com/macrosystems/dailynews/ui/factory/NewsFeedFragmentFactory.kt
@@ -7,10 +7,15 @@ import com.macrosystems.dailynews.core.dialog.launcher.DialogFragmentLauncher
 import com.macrosystems.dailynews.ui.adapters.NewsFeedAdapter
 import com.macrosystems.dailynews.ui.view.fragments.DetailedNewsFragment
 import com.macrosystems.dailynews.ui.view.fragments.NewsFeedFragment
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
-//Creating a FragmentFactory we are able to inject constructors in the fragments.
-//Visit https://developer.android.com/reference/androidx/fragment/app/FragmentFactory
+/**Creating a FragmentFactory we are able to inject constructors in the fragments.
+Visit https://developer.android.com/reference/androidx/fragment/app/FragmentFactory*/
+
 class NewsFeedFragmentFactory @Inject constructor(
     private val newsFeedAdapter: NewsFeedAdapter,
     private val glide: RequestManager,
@@ -19,15 +24,14 @@ class NewsFeedFragmentFactory @Inject constructor(
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
-            NewsFeedFragment::class.java.name -> {
+            NewsFeedFragment::class.java.name ->
                 NewsFeedFragment(newsFeedAdapter, dialogFragmentLauncher, glide)
-            }
-            DetailedNewsFragment::class.java.name -> {
+
+            DetailedNewsFragment::class.java.name ->
                 DetailedNewsFragment(glide, dialogFragmentLauncher)
-            }
-            else -> {
+
+            else ->
                 super.instantiate(classLoader, className)
-            }
         }
     }
 }
