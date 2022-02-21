10d08e61e6bf4c2d2ce67880881e559cd84ff7ec
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2022-02-21 01:47:10 +0100

    test dispatchers are created

---
+++ app/src/test/java/com/macrosystems/dailynews/core/providers/TestDispatchers.kt
@@ -0,0 +1,16 @@
+package com.macrosystems.dailynews.core.providers
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+
+@ExperimentalCoroutinesApi
+class TestDispatchers: DefaultDispatchers() {
+    val testDispatcher = UnconfinedTestDispatcher()
+    override val main: CoroutineDispatcher
+        get() = testDispatcher
+    override val io: CoroutineDispatcher
+        get() = testDispatcher
+    override val default: CoroutineDispatcher
+        get() = testDispatcher
+}
